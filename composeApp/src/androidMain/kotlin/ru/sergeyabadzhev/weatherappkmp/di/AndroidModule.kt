7790132b2ae2f4861c1f3da8d0fad6d9ed25de5f
Sergey Abadzhev <q3er@mail.ru>
2026-03-18 15:41:47 +0300

    koin dsl

--- composeApp/src/androidMain/kotlin/ru/sergeyabadzhev/weatherappkmp/di/AndroidModule.kt
+++ composeApp/src/androidMain/kotlin/ru/sergeyabadzhev/weatherappkmp/di/AndroidModule.kt
@@ -2,9 +2,13 @@ package ru.sergeyabadzhev.weatherappkmp.di
 
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.bind
 import org.koin.dsl.module
+import org.koin.plugin.module.dsl.single
+import org.koin.plugin.module.dsl.viewModel
 import ru.sergeyabadzhev.weatherappkmp.core.location.GeocoderService
 import ru.sergeyabadzhev.weatherappkmp.core.location.LocationProvider
+import ru.sergeyabadzhev.weatherappkmp.core.location.LocationProviderInterface
 import ru.sergeyabadzhev.weatherappkmp.core.storage.CityStorage
 import ru.sergeyabadzhev.weatherappkmp.core.storage.CityStorageInterface
 import ru.sergeyabadzhev.weatherappkmp.core.storage.LocationPreferences
@@ -13,11 +17,8 @@ import ru.sergeyabadzhev.weatherappkmp.features.home.HomeViewModel
 import ru.sergeyabadzhev.weatherappkmp.features.search.SearchViewModel
 
 val androidModule = module {
-    single { GeocoderService(androidContext()) }
-    single { LocationProvider(androidContext()) }
-    single<LocationPreferencesInterface> { LocationPreferences(androidContext()) }
-    single<CityStorageInterface> { CityStorage(androidContext()) }
-
-    viewModel { HomeViewModel(get(), get(), get()) }
-    viewModel { SearchViewModel(get(), get()) }
+    single<GeocoderService>()
+    single<LocationProvider>() bind LocationProviderInterface::class
+    single<LocationPreferences>() bind LocationPreferencesInterface::class
+    single<CityStorage>() bind CityStorageInterface::class
 }
--- shared/src/commonMain/kotlin/ru/sergeyabadzhev/weatherappkmp/di/SharedModule.kt
+++ shared/src/commonMain/kotlin/ru/sergeyabadzhev/weatherappkmp/di/SharedModule.kt
@@ -1,14 +1,22 @@
 package ru.sergeyabadzhev.weatherappkmp.di
 
+import org.koin.dsl.bind
 import org.koin.dsl.module
+import org.koin.plugin.module.dsl.single
+import org.koin.plugin.module.dsl.viewModel
 import ru.sergeyabadzhev.weatherappkmp.core.network.NetworkClient
 import ru.sergeyabadzhev.weatherappkmp.data.repository.CityRepositoryImpl
 import ru.sergeyabadzhev.weatherappkmp.data.repository.WeatherRepositoryImpl
 import ru.sergeyabadzhev.weatherappkmp.domain.repository.CityRepository
 import ru.sergeyabadzhev.weatherappkmp.domain.repository.WeatherRepository
+import ru.sergeyabadzhev.weatherappkmp.features.home.HomeViewModel
+import ru.sergeyabadzhev.weatherappkmp.features.search.SearchViewModel
 
 val sharedModule = module {
-    single { NetworkClient() }
-    single<CityRepository> { CityRepositoryImpl(get()) }
-    single<WeatherRepository> { WeatherRepositoryImpl(get(), get()) }
+    single<NetworkClient>()
+    single<CityRepositoryImpl>() bind CityRepository::class
+    single<WeatherRepositoryImpl>() bind WeatherRepository::class
+
+    viewModel<HomeViewModel>()
+    viewModel<SearchViewModel>()
 }
